68069bd617d4cb29fa439245f459d256f654f894
Rits5 <44517216+Rits5@users.noreply.github.com>
2020-05-14 23:05:38 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # UAS-Challenge
 UAS pre-interview line-circle Intersection challenge.
 
+### Question
+Given 2 points on a line, the coordinate of the center of a circle, and the radius of the circle, determine if the line intersects with the circle once, twice, or zero times.
+
+[View the code](../line-circle-itersection/main.cpp)
+
 
--- line-circle-intersection/main.cpp
+++ line-circle-intersection/main.cpp
@@ -1,7 +1,7 @@
 /*
 	Purpose: The program identifies if there will be 0, 1, or 2 intersection between
-			 a line and circle and indicates the coordinates of intersection if there
-			 are any.
+		a line and circle and indicates the coordinates of intersection if there
+		are any.
 */
 
 #include <iostream>
@@ -66,14 +66,11 @@ void lineCircleIntersection(double x1, double y1, double x2, double y2, double c
 
 int main() {
 
-	lineCircleIntersection(0, 10, 30, 10, 12, 0, 10);  // Line: (0,10), (30,10)
-													   //Circle: (12, 0) r = 10
+	lineCircleIntersection(0, 10, 30, 10, 12, 0, 10);  // Line: (0,10), (30,10); Circle: (12, 0) r = 10
 
-	lineCircleIntersection(0, -10, 15, 15, 9, 3, 5);   // Line: (0,-10), (15,15)
-													   //Circle: (9, 3) r = 5
+	lineCircleIntersection(0, -10, 15, 15, 9, 3, 5);   // Line: (0,-10), (15,15); Circle: (9, 3) r = 5
 
-	lineCircleIntersection(0, -10, 15, 15, 10, -5, 4); // Line: (0,-10), (15,15)
-													   //Circle: (10, -5) r = 4
+	lineCircleIntersection(0, -10, 15, 15, 10, -5, 4); // Line: (0,-10), (15,15); Circle: (10, -5) r = 4
 
 	return 0;
 }
